92b58de819925ad0005ebdf2b2a119fa362dd77d
Sh1nchaan <109457845+Sh1nchaan@users.noreply.github.com>
2024-11-24 14:47:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '919167545997';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '9167545997';  // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '919167545997';  // put your number here
 
 const ownerlist = ownervb.split(';');
 
